5e847134c2cf1901f3d0029f5175115ba9c864d0
norm <tkwpvk789@gmail.com>
2021-01-14 02:29:47 +0900

    이게 시간초과가 안나네

---
+++ boj_18119.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+inline int get_known_words(vector<int>& dict, int mask){
+
+    int forgotten_workds = 0;
+    for(int alpah : dict){
+        if(alpah & mask) forgotten_workds += 1;
+    }
+
+    return dict.size() - forgotten_workds;
+}
+
+int main(){
+    //ios::sync_with_stdio(false);
+
+    int n,m;
+    vector<int> alpha;
+    int mask = 0;
+    cin >> n >> m;
+    alpha.resize(n); 
+
+    for(int i=0;i<n;i++){
+        string s; cin >> s;
+        for(char c : s){
+            alpha[i] |= (1<<(c-'a'));
+        }
+    }
+
+
+    for(int i=0;i<m;i++){
+        int op;
+        char target;
+        cin >> op >> target;
+        mask ^= (1<<(target - 'a'));
+
+        cout<< get_known_words(alpha, mask)<<"\n";
+    }
+
+    
+
+    return 0;
+}
